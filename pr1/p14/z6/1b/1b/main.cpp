f0e33bdf969de9e7dfbd75dc81b802c14b014d8f
rikire <070998nmis@mail.ru>
2019-11-21 18:55:20 +0500

    bibos

---
+++ pr1/p14/z6/1b/1b/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "     (*)" << endl;
+    cout << "   (*)0(*)" << endl;
+    cout << "  (*)(*)(*)" << endl;
+    cout << "   \\XXXXX/" << endl;
+    cout << "    \\XXX/" << endl;
+    cout << "     \\X/" << endl;
+    cout << "      V" << endl;
+    return 0;
+}
